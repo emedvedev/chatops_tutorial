b962dc5aab4327c3f25f4649f8bcfa33782e391a
Edward Medvedev <edward.medvedev@gmail.com>
2016-03-11 22:12:53 +0600

    Pizza ordering alias and action

---
+++ actions/pizza.py
@@ -0,0 +1,14 @@
+import requests
+
+from st2actions.runners.pythonrunner import Action
+
+
+class PizzaAction(Action):
+    def __init__(self, config=None):
+        super(PizzaAction, self).__init__(config=config)
+
+    def run(self):
+        return requests.post(
+            self.config.pizza.order_page,
+            data=self.config.pizza.payload
+        ).text
